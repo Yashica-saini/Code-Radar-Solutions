e757e75e4d04f33b1fe02442630b307e127d10d4
Yashica-saini <yashicasixa64@gmail.com>
2025-02-01 21:38:49 +0530

    Solution to Clear nth Bit of a Number

--- Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
+++ Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdiolib.h>
-
 int main() {
     int a,b;
-    scanf("%d &d",&a,&b);
+    scanf("%d %d",&a,&b);
     int c=~(b>>1);
     printf("%d",c&a);
     return 0;
