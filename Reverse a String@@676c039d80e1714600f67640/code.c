51966396df2d3b57c7cf904b87e7540fbf4035c3
Yashica Saini <yashicasixa64@gmail.com>
2025-03-15 14:48:14 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    char str;
-    scanf("%c",&str);
-    if(str!='\n'){
-        main();
-        printf("%c",str);
+
+int main() {
+    char str[100];  
+    scanf("%s", str);  // Read string input
+    int len = strlen(str);  
+
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);  // Print characters in reverse order
     }
-    
-    return 0;}
+
+    return 0;
+}
