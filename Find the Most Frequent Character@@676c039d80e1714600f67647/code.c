071a82abd5170fcfd6d34f039c2e485f0f267224
Yashica Saini <yashicasixa64@gmail.com>
2025-03-16 17:52:33 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,22 +1,23 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     char str[100];
-    scanf("%[^\n]",str);
-    int freq[256]={0};
-    int max=0;
-    char maxchar='\0';
-    for(int i=0;str[i]!=0;i++){
-        if(str[i]==' ')
-            continue;
-        
-        freq[(int)str[i]]++;
-        if(freq[(int)str[i]]>max){
-            max=freq[(int)str[i]];
-            maxchar=str[i];
+    scanf(" %[^\n]", str);  // Read input with spaces
+
+    int freq[256] = {0}, max = 0;
+    char maxchar = '\0';
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') continue;  // Skip spaces
+        freq[(int)str[i]]++;  
+
+        if (freq[(int)str[i]] > max) { 
+            max = freq[(int)str[i]];
+            maxchar = str[i];
         }
     }
-    printf("%c",maxchar);
-    return 0;
 
+    printf("%c\n", maxchar);  // Print most frequent character
+    return 0;
 }
