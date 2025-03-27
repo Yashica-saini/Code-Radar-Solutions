87f4a976a95018bf9d440576bd0fe0f2ef8a1a2a
Yashica Saini <yashicasixa64@gmail.com>
2025-03-28 03:04:24 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-// Partition function used in QuickSelect
+
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];  // Choose the last element as pivot
+    int pivot = arr[high];  
     int i = low - 1;
 
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
-            // Swap arr[i] and arr[j]
+            
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
     }
 
-    // Swap pivot to its correct position
+    
     int temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
@@ -23,23 +23,22 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
-// QuickSelect function to find Kth smallest element
+
 int quickSelect(int arr[], int low, int high, int k) {
     if (low <= high) {
         int pi = partition(arr, low, high);
 
-        if (pi == k - 1)  // Found the Kth smallest element
+        if (pi == k - 1)  
             return arr[pi];
 
-        if (k - 1 < pi)  // Search in the left subarray
+        if (k - 1 < pi) 
             return quickSelect(arr, low, pi - 1, k);
 
-        return quickSelect(arr, pi + 1, high, k);  // Search in the right subarray
+        return quickSelect(arr, pi + 1, high, k);  
+    return -1; 
     }
-    return -1; // This case should not happen for valid input
 }
 
-// Function to find the Kth smallest element
 int kthSmallest(int arr[], int n, int k) {
     return quickSelect(arr, 0, n - 1, k);
 }
