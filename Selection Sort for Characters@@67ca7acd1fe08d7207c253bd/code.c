0fccdea77cccfb9bca8871734bc6103bae3a909c
Yashica Saini <yashicasixa64@gmail.com>
2025-03-27 21:07:48 +0530

    Solution to Selection Sort for Characters

--- Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -4,11 +4,11 @@ void selectionSort(char arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) { // Find the smallest character
+            if (arr[j] < arr[minIndex]) { 
                 minIndex = j;
             }
         }
-        // Swap characters
+        
         char temp = arr[i];
         arr[i] = arr[minIndex];
         arr[minIndex] = temp;
