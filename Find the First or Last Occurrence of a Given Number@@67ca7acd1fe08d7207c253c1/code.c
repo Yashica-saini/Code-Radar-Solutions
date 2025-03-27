78151b7f0d28b6af4f953aa0344602ebb8ab75b9
Yashica Saini <yashicasixa64@gmail.com>
2025-03-28 03:13:41 +0530

    Solution to Find the First or Last Occurrence of a Given Number

---
+++ Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Function to find the first occurrence using Binary Search
+int firstOccurrence(int arr[], int n, int key) {
+    int left = 0, right = n - 1, result = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == key) {
+            result = mid; // Store result and move left
+            right = mid - 1;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return result;
+}
+
+// Function to find the last occurrence using Binary Search
+int lastOccurrence(int arr[], int n, int key) {
+    int left = 0, right = n - 1, result = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == key) {
+            result = mid; // Store result and move right
+            left = mid + 1;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return result;
+}
