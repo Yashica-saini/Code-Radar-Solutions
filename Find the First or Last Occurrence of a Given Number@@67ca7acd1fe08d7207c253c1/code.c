30628c84f97bccdbb32a7ee419bd79f914e4c400
Yashica Saini <yashicasixa64@gmail.com>
2025-03-28 03:15:53 +0530

    Solution to Find the First or Last Occurrence of a Given Number

--- Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
+++ Find the First or Last Occurrence of a Given Number@@67ca7acd1fe08d7207c253c1/code.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
 
-// Function to find the first occurrence using Binary Search
-int firstoccurrence(int arr[], int n, int key) {
+// Function to find first or last occurrence of target
+int findOccurrence(int arr[], int n, int target, char mode) {
     int left = 0, right = n - 1, result = -1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
-        if (arr[mid] == key) {
-            result = mid; // Store result and move left
-            right = mid - 1;
-        } else if (arr[mid] < key) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return result;
-}
 
-// Function to find the last occurrence using Binary Search
-int lastoccurrence(int arr[], int n, int key) {
-    int left = 0, right = n - 1, result = -1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        
-        if (arr[mid] == key) {
-            result = mid; // Store result and move right
-            left = mid + 1;
-        } else if (arr[mid] < key) {
+
+        if (arr[mid] == target) {
+            result = mid;  // Store index
+            if (mode == 'F') {
+                right = mid - 1;  // Move left for first occurrence
+            } else {
+                left = mid + 1;   // Move right for last occurrence
+            }
+        } else if (arr[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
+
     return result;
 }
