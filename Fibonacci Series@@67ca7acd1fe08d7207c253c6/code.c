4118037f2a95cafd0d8509b1feedbde7f502870f
Yashica Saini <yashicasixa64@gmail.com>
2025-03-28 03:25:13 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,6 +4,11 @@
 void fibonacciSeries(int n) {
     int first = 0, second = 1, next;
 
+    if (n == 1) {
+        printf("%d\n", first);  // Print only 0 if n is 1
+        return;
+    }
+
     // Print the first two terms
     printf("%d %d ", first, second);
 
