3b0462d4026666b36bf27482302267c90b14f8f7
Yashica Saini <yashicasixa64@gmail.com>
2025-03-28 13:49:07 +0530

    Solution to Find the First Non-Repeated Character

--- Find the First Non-Repeated Character@@676c039d80e1714600f67649/code.c
+++ Find the First Non-Repeated Character@@676c039d80e1714600f67649/code.c
@@ -1,19 +1,41 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char str[100];
-    char a;
-    a='\0';
-    scanf("%s",str);
-    int len=strlen(str);
-    for(int i=0;i<=len;i++){
-        if(str[i]!=a){
-        str[i]=a;
-        }
-        else{
-            str[1]=a;
+#include <stdio.h>
+#include <string.h>
+
+char findFirstNonRepeatedChar(char str[]) {
+    int count[256] = {0};  
+    int i;
+
+   
+    for (i = 0; str[i] != '\0'; i++) {
+        count[str[i]]++;
+    }
+
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (count[str[i]] == 1) {
+            return str[i];
         }
     }
-    printf("%s",a);
+
+    return '\0';  
+}
+
+int main() {
+    char str[100];
+
+    
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    
+    str[strcspn(str, "\n")] = '\0';
+
+    char result = findFirstNonRepeatedChar(str);
+    if (result != '\0') {
+        printf("The first non-repeated character is: %c\n", result);
+    } else {
+        printf("No non-repeated character found.\n");
+    }
+
     return 0;
 }
