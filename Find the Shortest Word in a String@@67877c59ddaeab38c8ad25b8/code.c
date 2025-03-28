1381d66250374e49c7af35cac1788df352b3dabd
Yashica Saini <yashicasixa64@gmail.com>
2025-03-28 14:07:19 +0530

    Solution to Find the Shortest Word in a String

--- Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
+++ Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
@@ -8,35 +8,36 @@ void findShortestWord(char str[]) {
         return;
     }
 
-    int minLength = n;
+    int minLength = n;    // Initially, assume the shortest word is the whole string
     char shortestWord[n + 1];
     int currentLength = 0;
     int wordStart = 0;
 
     for (int i = 0; i <= n; i++) {
-        if (str[i] == ' ' || str[i] == '\0') {  
-            if (currentLength < minLength && currentLength > 0) {
+        if (str[i] == ' ' || str[i] == '\0') {  // End of a word
+            if (currentLength < minLength) {
                 minLength = currentLength;
                 strncpy(shortestWord, &str[wordStart], currentLength);
-                shortestWord[currentLength] = '\0';
+                shortestWord[currentLength] = '\0';  // Null-terminate the shortest word
             }
             currentLength = 0;
-            wordStart = i + 1;
+            wordStart = i + 1;  // Start of next word
         } else {
             currentLength++;
         }
     }
 
-
-    printf("%s\n", shortestWord);
+    // Output the shortest word found
+    printf("The shortest word is: %s\n", shortestWord);
 }
 
 int main() {
     char str[1000];
 
-   
+    // Taking input
+    printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0';  
+    str[strcspn(str, "\n")] = '\0';  // Remove trailing newline if any
 
     findShortestWord(str);
 
