7933a31ddf59a977190ab61aa8c7d7132b4870e0
Yashica Saini <yashicasixa64@gmail.com>
2025-04-09 18:55:17 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -1,39 +1,35 @@
+/ Your code here...
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_BOOKS 100
-
-struct Book {
-    int id;
+typedef struct {
     char title[100];
     char author[100];
     float price;
-};
+} Book;
+
+void displayBooksAboveThreshold(Book books[], int n, float threshold) {
+    printf("Books above price %.2f:\n", threshold);
+    for (int i = 0; i < n; i++) {
+        if (books[i].price > threshold) {
+            printf("Title: %s, Author: %s, Price: %.2f\n", books[i].title, books[i].author, books[i].price);
+        }
+    }
+}
 
 int main() {
-    struct Book library[MAX_BOOKS];
     int n;
-
-    // Input: number of books
     scanf("%d", &n);
 
+    Book books[n];
+
     for (int i = 0; i < n; i++) {
-        scanf("%d", &library[i].id);
-        getchar();  // to consume newline after integer
-        fgets(library[i].title, sizeof(library[i].title), stdin);
-        library[i].title[strcspn(library[i].title, "\n")] = '\0';
-
-        fgets(library[i].author, sizeof(library[i].author), stdin);
-        library[i].author[strcspn(library[i].author, "\n")] = '\0';
-
-        scanf("%f", &library[i].price);
+        scanf("%s %s %f", books[i].title, books[i].author, &books[i].price);
     }
 
-    // Output
-    for (int i = 0; i < n; i++) {
-        printf("Book %d: %s by %s, Price = %.2f\n",
-               library[i].id, library[i].title, library[i].author, library[i].price);
-    }
+    float threshold;
+    scanf("%f", &threshold);
+
+    displayBooksAboveThreshold(books, n, threshold);
 
     return 0;
 }
