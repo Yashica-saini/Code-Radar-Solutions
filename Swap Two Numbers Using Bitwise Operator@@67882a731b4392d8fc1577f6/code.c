8768179c2f61ae352327eb7fd9a5a5deb5aeb9e7
Yashica Saini <yashicasixa64@gmail.com>
2025-04-09 23:18:22 +0530

    Solution to Swap Two Numbers Using Bitwise Operator

--- Swap Two Numbers Using Bitwise Operator@@67882a731b4392d8fc1577f6/code.c
+++ Swap Two Numbers Using Bitwise Operator@@67882a731b4392d8fc1577f6/code.c
@@ -3,17 +3,17 @@
 int main() {
     int a, b;
     
-    printf("Enter two numbers:\n");
+
     scanf("%d %d", &a, &b);
 
-    printf("Before swapping: a = %d, b = %d\n", a, b);
+  
 
     // Swapping using XOR
     a = a ^ b;
     b = a ^ b;  // Now b = original a
     a = a ^ b;  // Now a = original b
 
-    printf("After swapping: a = %d, b = %d\n", a, b);
+    printf("%d %d\n", a, b);
 
     return 0;
 }
